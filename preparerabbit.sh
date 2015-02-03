47e296f1acd2da13a583e9ce30692f19040d75c8
adrian <adrian@eyeos.com>
2015-02-03 17:34:07 +0100

    lel

--- preparerabbit.sh
+++ preparerabbit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 touch /var/lib/rabbitmq/.erlang.cookie
 echo $ERLANG_COOKIE > /var/lib/rabbitmq/.erlang.cookie
+chown rabbitmq.rabbitmq /var/lib/rabbitmq/.erlang.cookie
+chmod 400 /var/lib/rabbitmq/.erlang.cookie
 rabbitmqctl -n rabbit@rabbitmq add_vhost /sensu
 rabbitmqctl -n rabbit@rabbitmq add_user sensu mypass
 rabbitmqctl -n rabbit@rabbitmq set_permissions -p /sensu sensu ".*" ".*" ".*"
